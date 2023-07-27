c5fa8ad7376ae624b20c4825235f89d789b276ea
KAMAL RAJ P <96537249+iamkamalraj@users.noreply.github.com>
2023-07-28 01:04:56 +0530

    Update Lambda1.java

--- Lambda Expression/src/Lambda1.java
+++ Lambda Expression/src/Lambda1.java
@@ -1,24 +1,28 @@
+//Lambda expression '->' this is symbol 
+//it is an Anonymous method any name to it which is the implementation of functional Interface
+// It is used only on functional Interface //Used in only one method
+
+
 interface Mobile
 {
 	void show (int num1,int num2);
 }
-
-
-
 
 public class Lambda1 {
 
 	public static void main(String[] args) {
 
 		
-//		Mobile mob = new Mobile ()
-//				{
-//			public void show (int num1)
-//			{
+//		Mobile mob = new Mobile ()				//In Lambda only parameter here ()
+		
+//				{					//Removing these 3 lines 
+//			public void show (int num1)			//
+//			{						//
+		
 //				System.out.println(" in Show " + num1);
 //			}
 			
-		
+									//Lambda Expression
 //	Mobile mob =  num1 -> System.out.println(" in Show " + num1);
 //	for one variable we can put num1 without bracket but for two we should use
 		
