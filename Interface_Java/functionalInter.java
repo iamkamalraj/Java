308d27fd397dd16350e22ce5cf0292f68cba5d7b
KAMAL RAJ P <96537249+iamkamalraj@users.noreply.github.com>
2023-07-28 00:51:06 +0530

    Update functionalInter.java

--- Interface_Java/functionalInter.java
+++ Interface_Java/functionalInter.java
@@ -1,8 +1,11 @@
 
-@FunctionalInterface
+@FunctionalInterface		//Whenever we are implementing Functional Interface we should use this in the top of the method
 interface Demo
 {
-	void disp();
+	void disp();		//If single method is called functional method //It is abstract method
+				//One abstract method / Single Abstract Method (SAM) 
+				//Functional Interface -- class -- ananumous inner class lambda expression
+				//Implementaion of Functional Interface(Body) in Lambda Expression
 }
 
 class Alpha implements Demo
