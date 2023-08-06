0587cdedf9cf5b227beeb2540a995e418c443d64
KAMAL RAJ P <96537249+iamkamalraj@users.noreply.github.com>
2023-08-06 15:24:17 +0530

    Add files via upload

---
+++ Multithreading/src/MultithreadingLaunch1.java
@@ -0,0 +1,15 @@
+
+public class MultithreadingLaunch1 {
+
+		public static void main(String[] args) throws Exception	//Compile time error in Thread.sleep(2000); so throws Exception	
+	{
+	
+		System.out.println("Good Moring Misss");
+		
+		Thread.sleep(2000);		// Waits for 2Secs
+		
+		System.out.println("Good Moring Students");
+
+	}
+
+}
