646434691087441997a62c62431a9b167b5b71db
KAMAL RAJ P <96537249+iamkamalraj@users.noreply.github.com>
2023-08-27 13:17:57 +0530

    Update Solution.java

--- HkrRnk_Practice/Solution.java
+++ HkrRnk_Practice/Solution.java
@@ -597,3 +597,32 @@ public class Solution {
 
 }
 ************************************************************************************************************************************
+//#Hacker Rank #15
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		String A = sc.next();
+		String B = sc.next();
+
+		System.out.println(A.length() + B.length());
+		System.out.println(A.compareTo(B) > 0 ? "Yes" : "No");
+		System.out.println(capitalizeFirstLetter(A) + " " + capitalizeFirstLetter(B));
+		sc.close();
+	}
+
+	public static String capitalizeFirstLetter(String input) {
+		if (input == null || input.isEmpty()) {
+			return input;
+		}
+
+		char[] charArray = input.toCharArray();
+		charArray[0] = Character.toUpperCase(charArray[0]);
+		return new String(charArray);
+	}
+}
+************************************************************************************************************************************
