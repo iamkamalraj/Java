871c732dbd6e8234f15fa949a4715bf5e384c6f5
KAMAL RAJ P <96537249+iamkamalraj@users.noreply.github.com>
2023-08-30 20:58:51 +0530

    Update Solution.java

--- HkrRnk_Practice/Solution.java
+++ HkrRnk_Practice/Solution.java
@@ -682,3 +682,28 @@ public class Solution {
     }
 }
 ************************************************************************************************************************************
+#Hacker Rank #17
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		String A = sc.next();
+		String rev = "";
+		/* Enter your code here. Print output to STDOUT. */
+		for (int i = 0; i < A.length(); i++) {
+			rev += A.charAt(A.length() - i - 1);
+		}
+
+		if (A.equals(rev)) {
+			System.out.println("Yes");
+		} else {
+			System.out.println("No");
+		}
+	}
+}
+
+************************************************************************************************************************************
