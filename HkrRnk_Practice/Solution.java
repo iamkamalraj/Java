bf2c233e844a32b99f3db1af52c4baceb9aa57c1
KAMAL RAJ P <96537249+iamkamalraj@users.noreply.github.com>
2023-09-01 21:30:22 +0530

    Update Solution.java

--- HkrRnk_Practice/Solution.java
+++ HkrRnk_Practice/Solution.java
@@ -742,3 +742,27 @@ public class Solution {
 	}
 }
 ************************************************************************************************************************************
+#Hacker Rank #20
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String s = scan.nextLine();
+		// Write your code here.
+		s = s.trim();
+		String str[] = s.split("[^a-zA-Z]+");
+		if (s.length() == 0) {
+			System.out.println(0);
+		} else {
+			System.out.println(str.length);
+		}
+		for (String word : str) {
+			System.out.println(word);
+		}
+		scan.close();
+	}
+}
+************************************************************************************************************************************
