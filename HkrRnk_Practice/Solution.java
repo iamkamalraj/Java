349af22bac6b5211214fd1e12a5af584f9d48ece
KAMAL RAJ P <96537249+iamkamalraj@users.noreply.github.com>
2023-09-03 14:29:50 +0530

    Update Solution.java

--- HkrRnk_Practice/Solution.java
+++ HkrRnk_Practice/Solution.java
@@ -859,3 +859,25 @@ public class Solution {
 // false
 // false
 ************************************************************************************************************************************
+//#Hacker Rank #22 // Passes All Test cases
+import java.util.Scanner;
+
+class Solution{
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+
+    }
+}
+class MyRegex {
+    // Regular expression for validating IP address
+    // The pattern checks for 4 groups of numbers (A.B.C.D)
+    // Each group can be from 0 to 255 with leading zeros allowed, and the length of each group can't be greater than 3.
+    // Note: We need to escape the backslashes in the Java string, so "\\" represents a single backslash in the regular expression.
+    String pattern = "^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
+}
+************************************************************************************************************************************
