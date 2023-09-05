8cf586df5e2f4aa84cbdbd1f53185d846a6d921f
KAMAL RAJ P <96537249+iamkamalraj@users.noreply.github.com>
2023-09-05 20:01:26 +0530

    Update Solution.java

--- HkrRnk_Practice/Solution.java
+++ HkrRnk_Practice/Solution.java
@@ -1013,3 +1013,47 @@ public class Solution {
 //Invalid
 
 //************************************************************************************************************************************
+//#Hacker Rank #25
+import java.util.*;
+import java.util.regex.*;
+
+public class Solution {
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+		int testCases = Integer.parseInt(in.nextLine());
+		while (testCases > 0) {
+			String line = in.nextLine();
+
+			// Write your code here
+			Pattern pattern = Pattern.compile("<([^<>/]+)>([^<>]+)</(\\1)>");
+			Matcher matcher = pattern.matcher(line);
+			if (matcher.find()) {
+				System.out.println(matcher.group(2));
+				while (matcher.find()) {
+					System.out.println(matcher.group(2));
+				}
+			} else {
+				System.out.println("None");
+			}
+
+			testCases--;
+		}
+	}
+}
+
+//Sample Input
+//
+//4
+//<h1>Nayeem loves counseling</h1>
+//<h1><h1>Sanjay has no watch</h1></h1><par>So wait for a while</par>
+//<Amee>safat codes like a ninja</amee>
+//<SA premium>Imtiaz has a secret crush</SA premium>
+//
+//Sample Output
+//
+//Nayeem loves counseling
+//Sanjay has no watch
+//So wait for a while
+//None
+//Imtiaz has a secret crush
