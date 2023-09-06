096ec27077e996e6e99b1fb4e434dbe2f5853b01
KAMAL RAJ P <96537249+iamkamalraj@users.noreply.github.com>
2023-09-06 16:52:32 +0530

    Update Solution.java

--- HkrRnk_Practice/Solution.java
+++ HkrRnk_Practice/Solution.java
@@ -1069,4 +1069,73 @@ public class Solution {
 //to match HTML end tag. (\1) to match the same content as in the first group. In the example, it is h1
 
 //************************************************************************************************************************************
+//#Hacker Rank #26
+import java.math.BigDecimal;
+import java.util.*;
+class Solution{
+    public static void main(String []args){
+        //Input
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        String []s=new String[n+2];
+        for(int i=0;i<n;i++){
+            s[i]=sc.next();
+        }
+        sc.close();
+
+        //Write your code here
+        for (int i = 0; i < n-1; i++) 
+    {
+        int minIdx =i;
+       for (int j = i+1; j < n; j++) 
+       {
+          String s1 = s[minIdx];
+          String s2 = s[j];
+          BigDecimal b1 = new BigDecimal(s1);
+          BigDecimal b2 = new BigDecimal(s2);
+          if(b1.compareTo(b2)<0)
+          {
+              minIdx = j;
+          }
+       }
+       String temp = s[i];
+       s[i] = s[minIdx];
+       s[minIdx] = temp;
+    }
+
+        
+
+        //Output
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(s[i]);
+        }
+    }
+}
+
+// Sample Input
+
+// 9
+// -100
+// 50
+// 0
+// 56.6
+// 90
+// 0.12
+// .12
+// 02.34
+// 000.000
+	
+// Sample Output
+
+// 90
+// 56.6
+// 50
+// 02.34
+// 0.12
+// .12
+// 0
+// 000.000
+// -100
+//************************************************************************************************************************************
 
