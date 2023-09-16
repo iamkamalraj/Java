ea0106c8a32756eb410b5b5883fc2ea9289e2438
KAMAL RAJ P <96537249+iamkamalraj@users.noreply.github.com>
2023-09-16 16:00:48 +0530

    Update Solution.java

--- HkrRnk_Practice/Solution.java
+++ HkrRnk_Practice/Solution.java
@@ -1295,5 +1295,97 @@ public class Solution {
 // Do not use method overloading because your answer will not be accepted.
 
 //**************************************************************************************************************************************************************
+//#Hacker Rank #Q31
+import java.io.*;
+import java.util.*;
 
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<List<Integer>> arr = new ArrayList<>();
+
+        for (int i = 0; i < 6; i++) {
+            String[] arrRowTempItems = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+            List<Integer> arrRowItems = new ArrayList<>();
+
+            for (int j = 0; j < 6; j++) {
+                int arrItem = Integer.parseInt(arrRowTempItems[j]);
+                arrRowItems.add(arrItem);
+            }
+
+            arr.add(arrRowItems);
+        }
+        //upto 4 because after 3 postition , including 4 , 5 only hourglass can be made, draw a matrix with x ,y values you will understand
+        int greatest=-100;
+        int hourg=0;
+       
+        for(int x=0;x<4;x++){
+            for(int y=0;y<4;y++){
+                hourg+=arr.get(x).get(y)+arr.get(x).get(y+1)+arr.get(x).get(y+2);
+                hourg+=arr.get(x+1).get(y+1);
+                hourg+=arr.get(x+2).get(y)+arr.get(x+2).get(y+1)+arr.get(x+2).get(y+2);
+             
+                if(hourg>greatest){
+                greatest=hourg;
+                }
+                
+                hourg=0;
+            }
+            
+  
+            
+        }
+        System.out.println(greatest);
+        bufferedReader.close();
+    }
+}
+
+//Sample Input
+//
+//1 1 1 0 0 0
+//0 1 0 0 0 0
+//1 1 1 0 0 0
+//0 0 2 4 4 0
+//0 0 0 2 0 0
+//0 0 1 2 4 0
+//
+//Sample Output
+//
+//19
+//
+//Question
+//You are given a 6*6 2D array. An hourglass in an array is a portion shaped like this:
+//
+//a b c
+//  d
+//e f g
+//For example, if we create an hourglass using the number 1 within an array full of zeros, it may look like this:
+//
+//1 1 1 0 0 0
+//0 1 0 0 0 0
+//1 1 1 0 0 0
+//0 0 0 0 0 0
+//0 0 0 0 0 0
+//0 0 0 0 0 0
+//Actually, there are many hourglasses in the array above. The three leftmost hourglasses are the following:
+//
+//1 1 1     1 1 0     1 0 0
+//  1         0         0
+//1 1 1     1 1 0     1 0 0
+//The sum of an hourglass is the sum of all the numbers within it. The sum for the hourglasses above are 7, 4, and 2, respectively.
+//
+//In this problem you have to print the largest sum among all the hourglasses in the array.
+//
+//Input Format
+//
+//There will be exactly  lines, each containing  integers seperated by spaces. Each integer will be between  and  inclusive.
+//
+//Output Format
+//
+//Print the answer to this problem on a single line.
+//**************************************************************************************************************************************************************
 
