a9bb6486909d1b8f1921059ffdab58ff4a93d661
KAMAL RAJ P <96537249+iamkamalraj@users.noreply.github.com>
2023-09-19 19:08:08 +0530

    Update Solution.java

--- HkrRnk_Practice/Solution.java
+++ HkrRnk_Practice/Solution.java
@@ -1389,3 +1389,35 @@ public class Solution {
 //Print the answer to this problem on a single line.
 //**************************************************************************************************************************************************************
 //#Hacker Rank #Q32
+mport java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+       Scanner sc = new Scanner(System.in);
+    int n =sc.nextInt();
+    int arr[] = new int[n];
+    for(int i=0; i<n; i++)
+    {
+        arr[i] = sc.nextInt();
+    }
+    int count = 0;
+
+    for(int i=0; i<n; i++)
+    {
+        int sum = 0;
+        for(int j=i; j<n; j++)
+        {
+            sum = sum + arr[j];
+            if(sum<0)
+            {
+                count++;
+            }
+        }
+    }
+    System.out.println(count);
+}
+}
