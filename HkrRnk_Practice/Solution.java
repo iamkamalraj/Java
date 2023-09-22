594a8d9a2076f8f0c9524c6469ebaf0fa19e9593
KAMAL RAJ P <96537249+iamkamalraj@users.noreply.github.com>
2023-09-22 22:54:05 +0530

    Update Solution.java

--- HkrRnk_Practice/Solution.java
+++ HkrRnk_Practice/Solution.java
@@ -1461,3 +1461,36 @@ Sample Input
 // Thus, we print  on a new line.
 //**************************************************************************************************************************************************************
 //#Hacker Rank #Q33
+	import java.util.*;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		ArrayList<ArrayList> list = new ArrayList<ArrayList>();
+		for (int i = 0; i < n; i++) {
+			int limit = sc.nextInt();
+			ArrayList<Integer> list2 = new ArrayList<Integer>();
+
+			for (int j = 0; j < limit; j++) {
+
+				list2.add(sc.nextInt());
+			}
+			list.add(list2);
+		}
+		int q = sc.nextInt();
+		for (int i = 0; i < q; i++) {
+			int x = sc.nextInt();
+			int y = sc.nextInt();
+			try {
+				System.out.println((list.get(x - 1)).get(y - 1));
+			} catch (Exception e) {
+				System.out.println("ERROR!");
+			}
+
+		}
+
+	}
+}
+
