25f879b1ecf90b59343888d5cd7f75bcd602361c
KAMAL RAJ P <96537249+iamkamalraj@users.noreply.github.com>
2023-09-28 20:18:22 +0530

    Update Solution.java

--- HkrRnk_Practice/Solution.java
+++ HkrRnk_Practice/Solution.java
@@ -1544,6 +1544,47 @@ public class Solution {
 //Q34
 
 
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    ArrayList<ArrayList>list=new ArrayList<ArrayList>();
+    for(int i=0;i<n;i++)
+    {
+        int limit=sc.nextInt();
+         ArrayList<Integer>list2=new ArrayList<Integer>();
+
+        for(int j=0;j<limit;j++)
+         {
+
+             list2.add(sc.nextInt());
+         }
+         list.add(list2);
+    }
+    int q=sc.nextInt();
+    for(int i=0;i<q;i++)
+    {
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        try{
+            System.out.println((list.get(x-1)).get(y-1));
+        }
+        catch(Exception e)
+        {
+            System.out.println("ERROR!");
+        }
+
+
+    }
+
+    }
+}
+
+
+
 // Sometimes it's better to use dynamic size arrays. Java's Arraylist can provide you this feature. Try to solve this problem using Arraylist.
 
 // You are given  lines. In each line there are zero or more integers. You need to answer a few queries where you need to tell the number located in  position of  line.
