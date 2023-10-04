8feec6e277357807e505d2ae22ef5d981c924060
KAMAL RAJ P <96537249+iamkamalraj@users.noreply.github.com>
2023-10-04 20:12:15 +0530

    Update Solution.java

--- HkrRnk_Practice/Solution.java
+++ HkrRnk_Practice/Solution.java
@@ -1653,6 +1653,20 @@ public class Solution {
 	}
 }
 //**************************************************************************************************************************************************************
-//#Hacker Rank Q35
+#Hacker Rank Q35
+In this problem, you have to add and multiply huge numbers! These numbers are so big that you can't contain them in any ordinary data types like a long integer.
 
+Use the power of Java's BigInteger class and solve this problem.
+
+Input Format
+
+There will be two lines containing two numbers,  and .
+
+Constraints
+
+ and  are non-negative integers and can have maximum  digits.
+
+Output Format
+
+Output two lines. The first line should contain , and the second line should contain . Don't print any leading zeros.
 
