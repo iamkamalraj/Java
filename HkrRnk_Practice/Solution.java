b601ccc510641559d27156bf5445ffec8c54b62e
KAMAL RAJ P <kamalrajp.cse@gmail.com>
2023-10-12 20:04:57 +0530

    Update Solution.java

--- HkrRnk_Practice/Solution.java
+++ HkrRnk_Practice/Solution.java
@@ -1675,19 +1675,12 @@ Output two lines. The first line should contain , and the second line should con
 //**************************************************************************************************************************************************************
 #Hacker Rank Q35
 // In this problem, you have to add and multiply huge numbers! These numbers are so big that you can't contain them in any ordinary data types like a long integer.
-
 // Use the power of Java's BigInteger class and solve this problem.
-
 // Input Format
-
 // There will be two lines containing two numbers, a and b .
-
 // Constraints
-
 //  a and b are non-negative integers and can have maximum 200 digits.
-
 // Output Format
-
 // Output two lines. The first line should contain a + b, and the second line should contain a x b . Don't print any leading zeros.
 
 
@@ -1712,6 +1705,7 @@ public class Solution {
         
     }
 }
+
 // Sample Input
 
 // 1234
