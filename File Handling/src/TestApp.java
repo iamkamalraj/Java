e3e2ebe25c73c4dc3838e4fe7356d68717bab72a
KAMAL RAJ P <kamalrajp.cse@gmail.com>
2023-10-21 19:27:19 +0530

    Update TestApp.java

--- File Handling/src/TestApp.java
+++ File Handling/src/TestApp.java
@@ -144,22 +144,22 @@ public class TestApp {
 //      To close the stream.
 //
 //eg#1.
-//import java.io.FileWriter;
-//import java.io.IOException;
-//
-//public class TestApp {
-//      public static void main(String[] args)throws IOException {
-//                  FileWriter fw=new FileWriter("abc.txt");
-//                  fw.write(73);
-//                   fw.write("neuron\nTechnology\nPrivate\nLimited");
-//                   fw.write("\n");
-//                   char ch[] ={'a','b','c'};
-//                   fw.write(ch);
-//                   fw.flush();
-//                   fw.close();
-//     }
-//}
-//
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class TestApp {
+     public static void main(String[] args)throws IOException {
+                 FileWriter fw=new FileWriter("abc.txt");
+                 fw.write(73);
+                  fw.write("neuron\nTechnology\nPrivate\nLimited");
+                  fw.write("\n");
+                  char ch[] ={'a','b','c'};
+                  fw.write(ch);
+                  fw.flush();
+                  fw.close();
+    }
+}
+
 //A new file will be created automatically
 //
 //abc.txt
