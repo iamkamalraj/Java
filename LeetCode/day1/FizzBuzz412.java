ea8f5448a2ea2ea2f98c1bc1250c6ff091565e8c
KAMAL RAJ P <kamalrajp.cse@gmail.com>
2023-11-01 22:32:54 +0530

    Update FizzBuzz412.java

--- LeetCode/day1/FizzBuzz412.java
+++ LeetCode/day1/FizzBuzz412.java
@@ -1,10 +1,6 @@
 package day1;
 
 //412. Fizz Buzz
-//Solved
-//Easy
-//Topics
-//Companies
 //Given an integer n, return a string array answer (1-indexed) where:
 //
 //answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
@@ -28,7 +24,6 @@ package day1;
 // 
 //
 //Constraints:
-//
 //1 <= n <= 104
 
 
@@ -89,3 +84,15 @@ class Solution {
         return answer;
     }
 }
+
+//Test Case 1
+//Input: n = 3
+//Output: ["1","2","Fizz"]
+
+//Test Case 2
+//Input: n = 5
+//Output: ["1","2","Fizz","4","Buzz"]
+
+//Test Case 3:
+//Input: n = 15
+//Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
