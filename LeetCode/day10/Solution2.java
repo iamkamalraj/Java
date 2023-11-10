e0c0bbbd486c9ea52b8fcaeee1e0ac6eb9067c82
KAMAL RAJ P <kamalrajp.cse@gmail.com>
2023-11-10 20:25:08 +0530

    Update Solution2.java

--- LeetCode/day10/Solution2.java
+++ LeetCode/day10/Solution2.java
@@ -31,6 +31,7 @@ package day10;
 //
 //Follow up: Recursive solution is trivial, could you do it iteratively?
 
+//https://youtu.be/6wxNc8gCj8E
 
 class Solution {
    private List<Integer> res = new ArrayList<>();
@@ -49,5 +50,38 @@ class Solution {
    }
 }
 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+// class Solution {
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//         List<Integer> result = new ArrayList<>();
+//         inorder(root, result);
+//         return result;
+//     }
 
-//https://youtu.be/6wxNc8gCj8E
+//     public void inorder(TreeNode root, List<Integer> result) {
+//         if (root == null) {
+//             return;
+//         }
+
+//         inorder(root.left, result);
+//         result.add(root.val);
+//         inorder(root.right, result);
+//     }
+// }
+
+
+
