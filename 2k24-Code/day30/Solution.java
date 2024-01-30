cf28633729512d87f7b96370e43ea81d523e767b
KAMAL RAJ P <kamalrajp.cse@gmail.com>
2024-01-30 19:28:19 +0530

    Add files via upload

---
+++ 2k24-Code/day30/Solution.java
@@ -0,0 +1,72 @@
+package day30;
+
+//150. Evaluate Reverse Polish Notation
+//
+//You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
+//
+//Evaluate the expression. Return an integer that represents the value of the expression.
+//
+//Note that:
+//
+//The valid operators are '+', '-', '*', and '/'.
+//Each operand may be an integer or another expression.
+//The division between two integers always truncates toward zero.
+//There will not be any division by zero.
+//The input represents a valid arithmetic expression in a reverse polish notation.
+//The answer and all the intermediate calculations can be represented in a 32-bit integer.
+// 
+//
+//Example 1:
+//
+//Input: tokens = ["2","1","+","3","*"]
+//Output: 9
+//Explanation: ((2 + 1) * 3) = 9
+//Example 2:
+//
+//Input: tokens = ["4","13","5","/","+"]
+//Output: 6
+//Explanation: (4 + (13 / 5)) = 6
+//Example 3:
+//
+//Input: tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+//Output: 22
+//Explanation: ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+//= ((10 * (6 / (12 * -11))) + 17) + 5
+//= ((10 * (6 / -132)) + 17) + 5
+//= ((10 * 0) + 17) + 5
+//= (0 + 17) + 5
+//= 17 + 5
+//= 22
+// 
+//
+//Constraints:
+//
+//1 <= tokens.length <= 104
+//tokens[i] is either an operator: "+", "-", "*", or "/", or an integer in the range [-200, 200].
+
+
+class Solution {
+    long resolves(long a, long b, char Operator) {
+        if (Operator == '+') return a + b;
+        else if (Operator == '-') return a - b;
+        else if (Operator == '*') return a * b;
+        return a / b;
+    }
+
+    public int evalRPN(String[] tokens) {
+        Stack<Long> stack = new Stack<>();
+        int n = tokens.length;
+        for (int i = 0; i < n; i++) {
+            if (tokens[i].length() == 1 && tokens[i].charAt(0) < 48) {
+                long integer2 = stack.pop();
+                long integer1 = stack.pop();
+                char operator = tokens[i].charAt(0);
+                long resolvedAns = resolves(integer1, integer2, operator);
+                stack.push(resolvedAns);
+            } else {
+                stack.push(Long.parseLong(tokens[i]));
+            }
+        }
+        return stack.pop().intValue();
+    }
+}
