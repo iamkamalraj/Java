688f6c4339af6949f65857d91fd99c9b12ba8880
KAMAL RAJ P <kamalrajp.cse@gmail.com>
2024-02-26 22:33:03 +0530

    Update Solution.java

--- 2k24-Code/day56/Solution.java
+++ 2k24-Code/day56/Solution.java
@@ -29,21 +29,6 @@ package day56;
 //The number of nodes in both trees is in the range [0, 100].
 //-104 <= Node.val <= 104
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if (p == null && q == null) return true;
